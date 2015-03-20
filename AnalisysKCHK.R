e029f9952df55f6b157bff5bf9fc9ff766eb8367
amchercashin <amchercashin@gmail.com>
2015-03-20 16:29:32 +0300

    add figures

--- AnalisysKCHK.R
+++ AnalisysKCHK.R
@@ -10,24 +10,20 @@ space <- function(x, ...) {format(x, ..., big.mark = " ", scientific = FALSE, tr
 
 ###Data reading and processing
 #Read fixed costs for fact 2014, select only several columns
-costs_f14 <- read.xlsx("./data/ÊîíòðîëüÔÁ.xlsx", sheet = 19)
-costs_f14 <- tbl_df(data.frame(companygroup = costs_f14[,11], scenario = "Ôàêò",
-                       month = costs_f14[,15], pl = costs_f14[,16], fixed.costs = costs_f14[,10], ico = costs_f14[,18]))
+costs_f14 <- read.xlsx("./data/ÐšÐ¾Ð½Ñ‚Ñ€Ð¾Ð»ÑŒÐ¤Ð‘.xlsx", sheet = 19)
+costs_f14 <- tbl_df(data.frame(companygroup = costs_f14[,11], scenario = "Ð¤Ð°ÐºÑ‚",
+                               month = costs_f14[,15], fixed.costs = costs_f14[,10], ico = costs_f14[,18]))
 
 #Read fixed costs for budget 2015, select only several columns
-costs_bp15 <- read.xlsx("./data/ÔÁ2015_Êîíñîëèäàöèÿ_âñå_çàòðàòû.xlsx", sheet = 8)
-costs_bp15 <- tbl_df(data.frame(companygroup = costs_bp15[,11], scenario = "Áþäæåò",
-                        month = costs_bp15[,15], pl = costs_bp15[,16], fixed.costs = costs_bp15[,10], ico = costs_bp15[,19]))
+costs_bp15 <- read.xlsx("./data/Ð¤Ð‘2015_ÐšÐ¾Ð½ÑÐ¾Ð»Ð¸Ð´Ð°Ñ†Ð¸Ñ_Ð²ÑÐµ_Ð·Ð°Ñ‚Ñ€Ð°Ñ‚Ñ‹.xlsx", sheet = 8)
+costs_bp15 <- tbl_df(data.frame(companygroup = costs_bp15[,11], scenario = "Ð‘ÑŽÐ´Ð¶ÐµÑ‚",
+                                month = costs_bp15[,15], fixed.costs = costs_bp15[,10], ico = costs_bp15[,19]))
 #Costs processing
 costs <- rbind(costs_f14, costs_bp15) #combine two data.frames
 costs <- costs[!is.na(costs$fixed.costs),] #filter out all NA in costs sums
-costs <- filter(costs, companygroup == "Ãðóïïà ÂÌÓ" | companygroup == "Ãðóïïà ÀÇÎÒ" | companygroup == "Ìèíåðàëüíûå óäîáðåíèÿ, ÎÀÎ"
-                | companygroup == "Ãðóïïà Ê×ÕÊ" | companygroup == "ÏÌÓ" ) #take only plants costs
-
-costs$plblock <- strtrim(costs$pl,2)
-#Filter out other expenses
-costs <- filter(costs, plblock != "OE" & plblock != "FE")
-costs <- filter(costs, ico == 0) #filter out all ICO costs
+costs <- filter(costs, companygroup == "Ð“Ñ€ÑƒÐ¿Ð¿Ð° Ð’ÐœÐ£" | companygroup == "Ð“Ñ€ÑƒÐ¿Ð¿Ð° ÐÐ—ÐžÐ¢" | companygroup == "ÐœÐ¸Ð½ÐµÑ€Ð°Ð»ÑŒÐ½Ñ‹Ðµ ÑƒÐ´Ð¾Ð±Ñ€ÐµÐ½Ð¸Ñ, ÐžÐÐž"
+                | companygroup == "Ð“Ñ€ÑƒÐ¿Ð¿Ð° ÐšÐ§Ð¥Ðš" | companygroup == "ÐŸÐœÐ£" ) #take only plants costs
+#costs <- filter(costs, ico == 0) #filter out all ICO costs
 costs$month <- dmy("01.01.1900") + days(costs$month) - days(2) #format dates to POSIXct
 #Summarise
 costs <- group_by(costs, scenario, companygroup, month) %>% summarise(fixed.costs = sum(fixed.costs))
@@ -38,16 +34,14 @@ costs <- gather(costs, indicator, value, -(scenario:month))
 marg <- read.xlsx("./data/DBmarginality.xlsx", sheet = 1)
 marg <- tbl_df(data.frame(companygroup = marg[,3], scenario = marg[,5],
                           month = marg[,16], revenue = marg[,32], marginal.profit = marg[,42], ico = marg[,45]))
-#filter out ICO
-marg <- filter(marg, ico != "ÂÃÎ")
 #Revenue and profit data processing
 marg$scenario <- toupper(marg$scenario) #all scenario charachters to upper case
 marg$month <- dmy("01.01.1900") + days(marg$month) - days(2) #date to POSIXct
 #Take only fact for 2014 and budget for 2015
-marg <- filter(marg, scenario == "ÁÏ" & month >= dmy("01.01.2015") | scenario == "ÔÀÊÒ" & month >= dmy("01.01.2014"))
+marg <- filter(marg, scenario == "Ð‘ÐŸ" & month >= dmy("01.01.2015") | scenario == "Ð¤ÐÐšÐ¢" & month >= dmy("01.01.2014"))
 marg <- filter(marg, companygroup != "SBU-Nitrotrade AG") #filter out non Uralchem company
-marg$scenario[marg$scenario == "ÔÀÊÒ"] <- "Ôàêò" #back to normal spelling
-marg$scenario[marg$scenario == "ÁÏ"] <- "Áþäæåò" #back to normal spelling
+marg$scenario[marg$scenario == "Ð¤ÐÐšÐ¢"] <- "Ð¤Ð°ÐºÑ‚" #back to normal spelling
+marg$scenario[marg$scenario == "Ð‘ÐŸ"] <- "Ð‘ÑŽÐ´Ð¶ÐµÑ‚" #back to normal spelling
 marg$revenue[is.na(marg$revenue)] <- 0 #all NA revenue to zeros
 marg$marginal.profit[is.na(marg$marginal.profit)] <- 0 #all NA marginal profit to zeros
 #Summarise
@@ -55,26 +49,26 @@ marg <- group_by(marg, scenario, companygroup, month) %>% summarise(revenue = su
 #To narrow tidy form
 marg <- gather(marg, indicator, value, -(scenario:month))
 marg$value <- marg$value * 1000 #from thousands to units
-
 #Combine two datasets
 data <- rbind(costs, marg)
 
 #Transform company group names to short names
 data$companygroup <- as.character(data$companygroup)
-data[grep("Ê×ÕÊ", data$companygroup, ignore.case = T), "companygroup"] <- "Ê×ÕÊ"
-data[grep("ÀÇÎÒ", data$companygroup, ignore.case = T), "companygroup"] <- "ÀÇÎÒ"
-data[grep("ÂÌÓ", data$companygroup, ignore.case = T), "companygroup"] <- "ÂÌÓ"
-data[grep("Ìèíåðàëüíûå óäîáðåíèÿ", data$companygroup, ignore.case = T), "companygroup"] <- "ÏÌÓ"
+data[grep("ÐšÐ§Ð¥Ðš", data$companygroup, ignore.case = T), "companygroup"] <- "ÐšÐ§Ð¥Ðš"
+data[grep("ÐÐ—ÐžÐ¢", data$companygroup, ignore.case = T), "companygroup"] <- "ÐÐ—ÐžÐ¢"
+data[grep("Ð’ÐœÐ£", data$companygroup, ignore.case = T), "companygroup"] <- "Ð’ÐœÐ£"
+data[grep("ÐœÐ¸Ð½ÐµÑ€Ð°Ð»ÑŒÐ½Ñ‹Ðµ ÑƒÐ´Ð¾Ð±Ñ€ÐµÐ½Ð¸Ñ", data$companygroup, ignore.case = T), "companygroup"] <- "ÐŸÐœÐ£"
 #Back companygroup names to factors 
-data$companygroup <- factor(data$companygroup, levels = c("ÀÇÎÒ", "Ê×ÕÊ", "ÏÌÓ", "ÂÌÓ"))
+data$companygroup <- factor(data$companygroup, levels = c("ÐÐ—ÐžÐ¢", "ÐšÐ§Ð¥Ðš", "ÐŸÐœÐ£", "Ð’ÐœÐ£"))
 #Indicators factor reoder
-data$indicator <- factor(data$indicator, levels = c("revenue", "marginal.profit", "fixed.costs"))
+data$indicator <- factor(data$indicator, levels = c("revenue", "marginal.profit", "fixed.costs")
+                         , lables = c("Ð’Ñ‹Ñ€ÑƒÑ‡ÐºÐ°", "ÐœÐ°Ñ€Ð¶Ð¸Ð½Ð°Ð»ÑŒÐ½Ð°Ñ Ð¿Ñ€Ð¸Ð±Ñ‹Ð»ÑŒ", "ÐŸÐ¾ÑÑ‚Ð¾ÑÐ½Ð½Ñ‹Ðµ Ð·Ð°Ñ‚Ñ€Ð°Ñ‚Ñ‹"))
 
 ##FIGURES
-my_grob = grobTree(textGrob("Ô 14", x=0.2,  y=0.85, hjust=0,
+my_grob = grobTree(textGrob("Ð¤ 14", x=0.2,  y=0.85, hjust=0,
                             gp=gpar(fontsize=22, col = "darkgrey", fontface = "bold")))
-my_grob2 = grobTree(textGrob("ÁÏ 15", x=0.8,  y=0.85, hjust=0,
-                            gp=gpar(fontsize=22, col = "darkgrey", fontface = "bold")))
+my_grob2 = grobTree(textGrob("Ð‘ÐŸ 15", x=0.8,  y=0.85, hjust=0,
+                             gp=gpar(fontsize=22, col = "darkgrey", fontface = "bold")))
 
 png("Plants_indicators.png", width=3308/2, height=2339/2)
 p <- ggplot(data, aes(x = month, y = value/10^6)) +
@@ -82,19 +76,19 @@ p <- ggplot(data, aes(x = month, y = value/10^6)) +
         geom_area(aes(fill = indicator), alpha = 0.65, position = "identity") +
         #geom_smooth(aes(colour = scenario), method="loess", se = F) +
         scale_y_continuous(labels = space) +
-        ylab("Ìèëëèîíîâ ðóáëåé") +
+        ylab("ÐœÐ¸Ð»Ð»Ð¸Ð¾Ð½Ð¾Ð² Ñ€ÑƒÐ±Ð»ÐµÐ¹") +
         xlab(NULL) +
-        ggtitle("Âûðó÷êà è ìàðæèíàëüíàÿ vs ïîñòîÿííûå çàòðàòû") +
+        ggtitle("Ð’Ñ‹Ñ€ÑƒÑ‡ÐºÐ° Ð¸ Ð¼Ð°Ñ€Ð¶Ð¸Ð½Ð°Ð»ÑŒÐ½Ð°Ñ vs Ð¿Ð¾ÑÑ‚Ð¾ÑÐ½Ð½Ñ‹Ðµ Ð·Ð°Ñ‚Ñ€Ð°Ñ‚Ñ‹") +
         theme_bw(base_size = 18) +
         theme(legend.position="right", legend.title=element_blank(), legend.key.size = unit(1.5,"cm")) +
         theme(axis.text=element_text(size=16), axis.title=element_text(size=18), strip.text.x=element_text(size=16), legend.text=element_text(size=16)) +
         theme(axis.text.x  = element_text(hjust=1,angle=90, vjust=0.5)) +
         scale_x_datetime(labels = date_format("%b.%y"), breaks = date_breaks("month"), minor_breaks = NULL) +
-        scale_fill_manual(values = c("#1b9e77", "#1f78b4", "#d95f02")) +
+        scale_fill_manual(values = c("#66CC99", "#9999CC", "#CC6666")) +
         geom_vline(xintercept = as.numeric(dmy("01.01.2015")), colour = "darkgrey", size = 1) +
         annotation_custom(my_grob) +
         annotation_custom(my_grob2) +
         coord_cartesian(ylim = c(0, 4.25*10^3), xlim = c(dmy("01.01.2014"), dmy("01.12.2015")))
-        
+
 print(p)
 dev.off()
